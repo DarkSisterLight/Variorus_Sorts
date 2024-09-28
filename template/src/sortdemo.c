e56076d5edaed400fec1a3b60077c2dac6b0bf03
Денис Бурлака <dburlaka@utrace.ru>
2024-09-29 01:22:48 +0300

    Шаблон проекта

---
+++ template/src/sortdemo.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+/**
+    Копирует содержимое из исходной области памяти в целевую область память
+    \param[out] dest Целевая область памяти
+    \param[in] src Исходная область памяти
+    \param[in] n Количество байтов, которые необходимо скопировать
+*/
+int* bubble_sort(int* src) {
+    return 0;
+}
+
+// Сортировка пузырьком
+int* insert_sort(int* src) {
+    return 0;
+}
+
+// Сортировка пузырьком
+int* merge_sort(int* src) {
+    return 0;
+}
+
+// Сортировка пузырьком
+int* quick_sort(int* src) {
+    return 0;
+}
+
+int main() {
+    printf("Hello!");
+    return 0;
+}
